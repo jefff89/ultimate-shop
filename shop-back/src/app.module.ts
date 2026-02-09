04e91b5c16fa618ec6665a5ba2406df24795f751
jefff89 <jeff.abbaszade@gmail.com>
2026-02-09 22:27:15 +0330

    user module in nest

--- shop-back/src/app.module.ts
+++ shop-back/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  // implementing typeorm
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true, // migrate codes and update tables automatically
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
