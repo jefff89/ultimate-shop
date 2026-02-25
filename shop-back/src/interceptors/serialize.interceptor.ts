435232534cecb1106c37537e6e0c2199de7e9719
jefff89 <jeff.abbaszade@gmail.com>
2026-02-25 21:40:29 +0330

    serializing responses

---
+++ shop-back/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,42 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): object; // any class is permitted as long as it is a class
+}
+
+// Our decorator for wrapping our interceptor
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+// our interceptor
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    handler: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // Run something before a request is handled by the request handler
+    console.log('Im running before the handler', context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        console.log('Im running before response is sent out', data);
+
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true, // only shows what is prfixed with Expose() in userDto
+        });
+      }),
+    );
+  }
+}
--- shop-back/src/users/users.controller.ts
+++ shop-back/src/users/users.controller.ts
@@ -12,7 +12,10 @@ import {
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user-dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 @Controller('auth')
+@Serialize(UserDto) // interceptor for sending requested response out without password included applied to all controller routes
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('/signup')
@@ -20,8 +23,11 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  // @UseInterceptors( new SerializeInterceptor(UserDto)) // for sending out responeses without including password
+  // @Serialize(UserDto) // instead of using long line above, we wrap the interceptor in a decorator
   @Get('/:id')
   async findUserById(@Param('id') id: string) {
+    console.log('handler is running'); // to see in SerializeInterceptor it runs after context and before handler logs there
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
